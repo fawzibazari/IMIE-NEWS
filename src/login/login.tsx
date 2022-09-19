8a423e9c1a30dd9f6ff23928d0910ae56232ce6a
fawzibazari <fawzi.bazari2@gmail.com>
2022-09-19 14:23:52 +0200

    working on it ...

---
+++ src/login/login.tsx
@@ -0,0 +1,17 @@
+function Login() {
+  return (
+    <div>
+      <p>login</p>
+      <div>
+        <label>Username</label><br/>
+        <input type="text" name="name" />
+      </div>
+      <div>
+        <label>Password</label><br/>
+        <input type="password" name="name" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
